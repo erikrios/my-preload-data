a8414dc25beadf183d42ec5b33a9a0b00b8484a3
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-07 11:58:39 +0700

    Integrated the view binding

--- app/src/main/java/com/erikriosetiawan/mypreloaddata/MahasiswaActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mypreloaddata/MahasiswaActivity.kt
@@ -2,10 +2,30 @@ package com.erikriosetiawan.mypreloaddata
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.erikriosetiawan.mypreloaddata.adapter.MahasiswaAdapter
+import com.erikriosetiawan.mypreloaddata.database.MahasiswaHelper
+import com.erikriosetiawan.mypreloaddata.databinding.ActivityMahasiswaBinding
 
 class MahasiswaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMahasiswaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mahasiswa)
+        binding = ActivityMahasiswaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        val mahasiswaAdapter = MahasiswaAdapter()
+        binding.recyclerView.adapter = mahasiswaAdapter
+
+        val mahasiswaHelper = MahasiswaHelper(this)
+        mahasiswaHelper.open()
+
+        val mahasiswaModels = mahasiswaHelper.getAllData()
+        mahasiswaHelper.close()
+
+        mahasiswaAdapter.setData(mahasiswaModels)
     }
 }
